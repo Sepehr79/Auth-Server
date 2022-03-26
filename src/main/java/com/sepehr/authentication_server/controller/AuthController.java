3a5bdbb9ad007108b1349520b2325d7702a826e3
Sepehr79 <sepehrmsm1379@gmail.com>
2022-03-26 11:29:54 +0430

    send mail

--- src/main/java/com/sepehr/authentication_server/controller/AuthController.java
+++ src/main/java/com/sepehr/authentication_server/controller/AuthController.java
@@ -3,7 +3,10 @@ package com.sepehr.authentication_server.controller;
 import com.sepehr.authentication_server.model.io.UserIO;
 import com.sepehr.authentication_server.model.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.util.Pair;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    @Value("${spring.mail.username}")
+    private String source;
+
     private final UserService userService;
+
+    private final JavaMailSender javaMailSender;
 
     @PostMapping("/users")
     public void temporarySaveUser(@RequestBody UserIO userIO){
         Pair<String, String> temporarySavingResult = userService.temporarySave(userIO);
-        // TODO send email
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(source);
+        mailMessage.setSubject("Authentication");
+        mailMessage.setTo(temporarySavingResult.getFirst());
+        mailMessage.setText("Your auth code: " + temporarySavingResult.getSecond());
+        javaMailSender.send(mailMessage);
     }
 
 }

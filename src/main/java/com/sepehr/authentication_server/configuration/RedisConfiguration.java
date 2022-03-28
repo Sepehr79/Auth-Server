c08b69ef2e28f57ad8b24f178907705acfa432ea
Sepehr79 <sepehrmsm1379@gmail.com>
2022-03-28 10:03:14 +0430

    redis configuration

--- src/main/java/com/sepehr/authentication_server/configuration/RedisConfiguration.java
+++ src/main/java/com/sepehr/authentication_server/configuration/RedisConfiguration.java
@@ -1,8 +1,10 @@
 package com.sepehr.authentication_server.configuration;
 
 import com.sepehr.authentication_server.model.entity.RedisUser;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -10,9 +12,18 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 @Configuration
 public class RedisConfiguration {
 
+    @Value("${redis.host}")
+    private String host;
+
+    @Value("${redis.port}")
+    private int port;
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory(){
-        return new JedisConnectionFactory();
+        var redisConfiguration = new RedisStandaloneConfiguration();
+        redisConfiguration.setHostName(host);
+        redisConfiguration.setPort(port);
+        return new JedisConnectionFactory(redisConfiguration);
     }
 
     @Bean

8659dfd264f3d8cafc691216dd219d535d89f09e
Sepehr79 <sepehrmsm1379@gmail.com>
2022-04-28 16:37:52 +0430

    refactoring

--- src/main/java/com/sepehr/authentication_server/controller/AuthController.java
+++ src/main/java/com/sepehr/authentication_server/controller/AuthController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 @RestController
 @RequestMapping("${api.path}")
